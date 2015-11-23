42036eb6b484aeaf8f162ab60ae5ed31b35afdd7
Govind Narute <govind.narute@mindbowser.com>
2015-11-23 17:59:42 +0530

    Forgot password and local storage

--- WebContent/js/controller/loginController.js
+++ WebContent/js/controller/loginController.js
@@ -4,8 +4,10 @@ myApp.controller("loginController", [
 		'$http',
 		'DRAFT_DYNASTY_URL',
 		'APIServices',
-		function($scope, $rootScope, $http, DRAFT_DYNASTY_URL, APIServices) {
+		'$localStorage',
+		function($scope, $rootScope, $http, DRAFT_DYNASTY_URL, APIServices,$localStorage) {
 			// This is for login user
+			$localStorage.myName="Govind Narute"
 			$scope.signIn = function() {
 				APIServices.validateLogin($scope.email, $scope.password)
 						.success(function(data, status) {
@@ -26,8 +28,10 @@ myApp.controller("loginController", [
 
 			// this is for temp
 			$scope.forgotPassword = function() {
-
-				APIServices.forgotPassword("govindnarute@gmail.com").success(
+				if(!$scope.email)
+				return	
+				 alert('test')
+				APIServices.forgotPassword($scope.email).success(
 						function(data, status) {
 							console.log("*********Response***********");
 							console.log(data);
--- WebContent/js/controller/signupController.js
+++ WebContent/js/controller/signupController.js
@@ -1,79 +1,77 @@
-myApp.controller("signupController", [
-		'$scope',
-		'$rootScope',
-		'$http',
-		'DRAFT_DYNASTY_URL',
-		'APIServices',
-		'$base64',
-		function($scope, $rootScope, $http, DRAFT_DYNASTY_URL, APIServices,
-				$base64) {
+myApp
+		.controller(
+				"signupController",
+				[
+						'$scope',
+						'$rootScope',
+						'$http',
+						'DRAFT_DYNASTY_URL',
+						'APIServices',
+						'$base64',
+						function($scope, $rootScope, $http, DRAFT_DYNASTY_URL,
+								APIServices, $base64) {
 
-			$scope.myImage = '';
-			$scope.myCroppedImage = '';
-			$scope.cropImageShow=false;
+							$scope.myImage = '';
+							$scope.myCroppedImage = '';
+							$scope.cropImageShow = false;
 
-			var handleFileSelect = function(evt) {
-				var file = evt.currentTarget.files[0];
-				var reader = new FileReader();
-				reader.onload = function(evt) {
-					$scope.$apply(function($scope) {
-						$scope.myImage = evt.target.result;
-						alert('test')
-						$scope.cropImageShow=true;
-						
-					});
-				};
-				reader.readAsDataURL(file);
-			};
-			angular.element(document.querySelector('#fileInput')).on('change',
-					handleFileSelect);
-			$scope.cropImage = function() {
-				//$scope.base64Image = $scope.myImage.split(",").pop();
-				// $scope.encoded = $base64.encode($scope.myCroppedImage);
-				$scope.base64Image = $scope.myCroppedImage.split(",").pop()
-				//
-				// $scope.base64Image=$scope.myCroppedImage;
+							var handleFileSelect = function(evt) {
+								var file = evt.currentTarget.files[0];
+								var reader = new FileReader();
+								reader.onload = function(evt) {
+									$scope.$apply(function($scope) {
+										$scope.myImage = evt.target.result;
+										$scope.cropImageShow = true;
 
-			};//cropImage()
+									});
+								};
+								reader.readAsDataURL(file);
+							};
+							angular.element(
+									document.querySelector('#fileInput')).on(
+									'change', handleFileSelect);
+							$scope.cropImage = function() {
+								// $scope.base64Image =
+								// $scope.myImage.split(",").pop();
+								// $scope.encoded =
+								// $base64.encode($scope.myCroppedImage);
+								$scope.base64Image = $scope.myCroppedImage
+										.split(",").pop()
+								//
+								// $scope.base64Image=$scope.myCroppedImage;
 
-			$scope.registerUser = function() {
-				alert('signup')
-				if ($scope.base64Image)
-					$scope.path = $scope.base64Image;
-				else
-					$scope.path = $scope.myImage.split(",").pop();
-				alert($scope.path)
-				APIServices.registerUser($scope.firstname, $scope.lastname,
-						$scope.path, $scope.email, $scope.password,
-						$scope.nickname).success(function(data, status) {
-					console.log("*********Response Register User***********");
-					console.log(data);
-					console.log(status);
-					alert('success');
-				}).error(function(data, status) {
+							};// cropImage()
 
-					console.log("*********Response Register User***********");
-					console.log(data);
-					console.log(status);
-					alert('faild');
-				});
-			};// register user()
+							$scope.registerUser = function() {
+								alert('signup')
+								if ($scope.base64Image)
+									$scope.path = $scope.base64Image;
+								else
+									$scope.path = $scope.myImage.split(",")
+											.pop();
+								alert($scope.path)
+								APIServices
+										.registerUser($scope.firstname,
+												$scope.lastname, $scope.path,
+												$scope.email, $scope.password,
+												$scope.nickname)
+										.success(
+												function(data, status) {
+													console
+															.log("*********Response Register User Success***********");
+													console.log(data);
+													console.log(status);
+													alert('success');
+												})
+										.error(
+												function(data, status) {
 
-			// APIServices.validateLogin("test", "test")
-			// .success(function(data, status) {
-			//					
-			// alert('success')
-			// }).error(function(data, status) {
-			//                      
-			// console.log("tttttttttttt")
-			// console.log(data)
-			// console.log(status)
-			// console.log("tttttttttttt")
-			// alert('faild')
-			// // console(status);
-			// // alert("Failed");
-			// });
-			//
-			// };
+													console
+															.log("*********Response Register User faild***********");
+													console.log(data);
+													console.log(status);
+													alert('faild');
+												});
+							};// register user()
 
-		} ]);
+						} ]);
